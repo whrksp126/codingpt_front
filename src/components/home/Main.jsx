040683b9caf69199e7e45828350e170f1ecf66ac
whrksp126 <whrksp126@gmail.com>
2025-01-03 02:00:15 +0900

    save

--- src/components/home/Main.jsx
+++ src/components/home/Main.jsx
@@ -3,11 +3,15 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import LottieAnimation from '../LottieAnimation';
 import Btn from '../component/Btn';
+import * as common from '../../utils/common';
+console.log(common.backendUrl)
+
 const Main = () => {
   const navigate = useNavigate();
   const handleStartClick = () => {
     navigate('/register');
   };
+  
   return (
     <div className="
       flex flex-col
